16fc54823bf33d16814523b338a6192f8f6418fe
Aayush Rathore <aayu.r.2003@gmail.com>
2024-05-08 23:24:40 +0530

    🚧 Currently working on Login page

--- todo_front-end/src/App.jsx
+++ todo_front-end/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Login from "./pages/auth/Login";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Start From Here</h1>
+        <Login />
       </div>
     </>
   );
